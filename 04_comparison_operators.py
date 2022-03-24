736daa97bd3e16655c46c89e94ff3feec935df7e
Kas234 <s.janczura@op.pl>
2022-03-24 17:31:10 +0100

    porównanie i operatory loogiczne

---
+++ 04_comparison_operators.py
@@ -0,0 +1,15 @@
+
+a = 20_000
+b = 20_000
+
+# print(a == b)
+# print(a != b) #nie jest równe
+# print(a > b)  #a jest większe od b
+# print(a < b)  #a jest mniejsze od b
+# print(a >= b) #a jest większe bądź równe b
+# print(a <= b) #a jest mniejsze bądź równe b
+
+#identity operator
+
+print(a is b) #nie używać - wywala błędy w konsoli
+
